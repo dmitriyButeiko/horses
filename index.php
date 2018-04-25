5deb42db3f3c8b90d03937c794a82d4806ec0618
Abhishek Jain <abhisheknjain11@gmail.com>
2018-04-25 12:58:45 +0530

    Added Horses Text file and modified code to read from txt file

--- index.php
+++ index.php
@@ -5,21 +5,39 @@ $username = "root";
 $password = "";
 $dbname = "horses";
 
+
+$file_handle = fopen("horses.txt", "rb");
+
+while (!feof($file_handle) ) {
+
+$line_of_text = fgets($file_handle);
+$parts = explode(',', $line_of_text);
+//print_r($parts);
+$str = join('", "', $parts);
+$str = "\"".$str."\"";
+    
+//echo $str;
+}
+
+fclose($file_handle);
+
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+/*
 if(isset($_REQUEST['searchsn'])){
     var_dump($_POST);
     $myArray = array_filter($_POST['sn']);
 $str = join('", "', $myArray);
 header('location:index.php?hname="'.$str.'"');
 }
-
-if(isset($_REQUEST['hname'])){
-$sql = "SELECT *, MIN(time) minimumtime,AVG(time) avgtime FROM data WHERE `name` IN (".$_REQUEST['hname'].") GROUP BY name,`distance`";
+*/
+if(isset($str)){
+$sql = "SELECT *, MIN(time) minimumtime,AVG(time) avgtime FROM data WHERE `name` IN (".$str.") GROUP BY name,`distance`";
 $result = $conn->query($sql);
 
 
@@ -99,7 +117,7 @@ $('#employee_grid').DataTable({
 </script>
 <?php } ?>
 
-<form method="POST" name="searchvanita" class="searchvanita" id="searchvanita">
+<form method="POST" name="searchvanita" class="searchvanita" id="searchvanita" style="display:none;">
 <div class="clear"></div>
 <div class="c_one"> <label>Horse 1</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
 <div class="c_one"> <label>Horse 2</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
