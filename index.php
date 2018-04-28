59b0ebf1ad4c86b113d3f4a2c2c5a274ded6922c
Abhishek Jain <abhisheknjain11@gmail.com>
2018-04-28 14:13:49 +0530

    Updates in Query for distance

--- index.php
+++ index.php
@@ -5,7 +5,54 @@ $username = "root";
 $password = "";
 $dbname = "horses";
 
+if(isset($_REQUEST['searchsn'])){
+    var_dump($_REQUEST);
+    $dtype = $_REQUEST['dtype'];
+    $distance = $_REQUEST['distance'];
+     $distance1 = $_REQUEST['distance1'];
+    switch ($dtype) {
+        case "Equals":
 
+            $sql1 = "distance = ".$distance;
+
+
+            break;
+  case "GreaterThan":
+
+            $sql1 = "distance > ".$distance;
+
+
+            break;
+         case "LessThan":
+
+            $sql1 = "distance < ".$distance;
+
+
+            break;
+         case "LessThanEquals":
+
+            $sql1 = "distance <= ".$distance;
+
+
+            break;
+         case "GreaterThanEquals":
+
+            $sql1 = "distance >= ".$distance;
+
+
+            break;
+         case "Between":
+
+            $sql1 = "distance BETWEEN ".$distance." AND ".$distance1;
+
+
+            break;
+        default:
+            echo "Default";
+            break;
+    }
+    //echo $sql1;
+   // exit();
 $file_handle = fopen("horses.txt", "rb");
 /*
 while (!feof($file_handle) ) {
@@ -15,7 +62,7 @@ $parts = explode(',', $line_of_text);
 //print_r($parts);
 $str = join('|', $parts);
 $str1 = "\"".$str."\"";
-    
+
 echo $str1;
 }*/
 //Change the distance as per your
@@ -29,7 +76,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 /*
 if(isset($_REQUEST['searchsn'])){
     var_dump($_POST);
@@ -61,12 +108,13 @@ $parts = explode(',', $line_of_text);
 //print_r($parts);
 $str = join('", "', $parts);
 $sql  .= "\"".$str."\"";
-    
 
-}
-$sql .= ") AND distance >= $distance GROUP BY name,`distance`";
 
-echo $sql;
+}$sql .= ") AND ";
+$sql .= $sql1;
+$sql .=  " GROUP BY name,`distance`";
+
+//echo $sql;
 
 $result = $conn->query($sql);
 
@@ -77,17 +125,17 @@ $result = $conn->query($sql);
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Datatable responsive with mysql</title>
+<title>Horses Data</title>
 <link rel="stylesheet" id="font-awesome-style-css" href="http://phpflow.com/code/css/bootstrap3.min.css" type="text/css" media="all">
 <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>
 
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css"/>
-	 
+
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/1.0.3/css/dataTables.responsive.css">
 <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/1.0.3/js/dataTables.responsive.js"></script>
 
-	
+
 	<div class="container">
       <div class="">
         <h1>Horse Data</h1>
@@ -101,7 +149,7 @@ $result = $conn->query($sql);
                 <th>distance</th>
                  <th>weight</th>
                   <th>Minimum Time</th>
-                 
+
                      <th>Average</th>
             </tr>
         </thead>
@@ -110,7 +158,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["name"]. "</td><td>" . $row["length"]. "</td><td>" . $row["condition"]. "</td><td>" . $row["distance"]. "</td><td>" . $row["weight"]. "</td><td>" . $row["minimumtime"]. "</td><td>" . $row["avgtime"]. "</td></tr>";
+        echo "<tr><td>" . $row["name"]. "</td><td>" . $row["length"]. "</td><td>" . $row["condition"]. "</td><td>" . $row["distance"]. "</td><td>" . $row["weight"]. "</td><td>" . $row["minimumtime"]. "</td><td>" . number_format($row["avgtime"],2). "</td></tr>";
     }
 } else {
     echo "0 results";
@@ -127,7 +175,7 @@ $conn->close();
                 <th>distance</th>
                  <th>weight</th>
                   <th>Minimum Time</th>
-                 
+
                      <th>Average</th>
             </tr>
         </tfoot>
@@ -140,32 +188,29 @@ $conn->close();
 <script type="text/javascript">
 $( document ).ready(function() {
 $('#employee_grid').DataTable({
-			
+
 		 "responsive": true,
-             });   
+             });
 });
 </script>
-
-<form method="POST" name="searchvanita" class="searchvanita" id="searchvanita" style="display:none;">
+<?php } ?>
+<form method="POST" name="searchvanita" class="searchvanita" id="searchvanita" >
 <div class="clear"></div>
-<div class="c_one"> <label>Horse 1</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 2</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 3</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 4</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 5</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 6</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 7</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 8</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 9</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 10</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 11</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 12</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 13</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 14</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 15</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 16</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-<div class="c_one"> <label>Horse 17</label> <input name="sn[]" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="Horse Name" autocomplete="off" />  </div>
-
+<div class="c_one"> <label>Distance</label>
+    <input name="distance" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="distance" autocomplete="off" />  </div>
+  
+<div class="c_one"> <label>Between Distance</label>
+    <input name="distance1" type="text" id="sn" tabindex="1" class="sm-form-c" placeholder="distance to" autocomplete="off" />  </div>
+ 
+    
+    <select name="dtype" id="type">
+            <option value="Equals">Equals</option>
+            <option value="GreaterThan">Greater Than</option>
+            <option value="LessThan">Less Than</option>
+            <option value="GreaterThanEquals">Greater Than Equals</option>
+               <option value="LessThanEquals">Less Than Equals</option>
+                <option value="Between">Between</option>
+        </select>
 <div class="col_one_fifth"> <br/> <input type="submit" name="searchsn" value="Search Horses"  tabindex="9" class="button button-3d nomargin" />
 </div>
 </form>
